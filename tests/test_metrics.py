3582070253ad30960e5e68e8681975d31dd8db9e
Yuu6798 <kkoo6798@gmail.com>
2025-05-21 00:41:14 +0900

    Ensure tests import project root

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 
 from secl_qa_cycle import novelty_score, is_duplicate_question, HistoryEntry
